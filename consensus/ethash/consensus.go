0c6a48749f64f848a7d1a5024208c36102badde9
Gyeonghun Park <zrhdnl@gmail.com>
2022-02-23 23:12:18 +0900

    consensus/ethash: fix block reward

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -39,11 +39,14 @@ import (
 
 // Ethash proof-of-work protocol constants.
 var (
-	FrontierBlockReward           = big.NewInt(5e+18) // Block reward in wei for successfully mining a block
-	ByzantiumBlockReward          = big.NewInt(3e+18) // Block reward in wei for successfully mining a block upward from Byzantium
-	ConstantinopleBlockReward     = big.NewInt(2e+18) // Block reward in wei for successfully mining a block upward from Constantinople
-	maxUncles                     = 2                 // Maximum number of uncles allowed in a single block
-	allowedFutureBlockTimeSeconds = int64(15)         // Max seconds from current time allowed for blocks, before they're considered future blocks
+	RewardString          = "50000000000000000000" //5e+19
+	DefaultBlockReward, _ = big.NewInt(0).SetString(RewardString, 10)
+
+	FrontierBlockReward           = DefaultBlockReward // Block reward in wei for successfully mining a block
+	ByzantiumBlockReward          = DefaultBlockReward // Block reward in wei for successfully mining a block upward from Byzantium
+	ConstantinopleBlockReward     = DefaultBlockReward // Block reward in wei for successfully mining a block upward from Constantinople
+	maxUncles                     = 2                  // Maximum number of uncles allowed in a single block
+	allowedFutureBlockTimeSeconds = int64(15)          // Max seconds from current time allowed for blocks, before they're considered future blocks
 
 	// calcDifficultyEip2384 is the difficulty adjustment algorithm as specified by EIP 2384.
 	// It offsets the bomb 4M blocks from Constantinople, so in total 9M blocks.
